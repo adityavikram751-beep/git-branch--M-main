bb4ccb856038e608aec6aadbee24c054d98b3c90
Aditya Vikram <adityavikram751@gmail.com>
2026-02-23 15:48:10 +0530

    euei

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://barber-syndicate.vercel.app/api/v1',
+  baseURL: 'https://barber-syndicate.vercel.app',
   headers: {
     'Content-Type': 'application/json',
   },
